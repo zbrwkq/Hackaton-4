82f8b5edf5bab0403fc1807fa612e33ff5404ba5
unknown <bensalahabdelrrahman1@gmail.com>
2024-05-28 16:29:45 +0200

    last commit v1

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -7,8 +7,10 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import axios from 'axios';
 
 import '../App.css';
+import API_URL from '../config.js';
 
 function TickParamsSelector({
   tickPlacement,
@@ -58,114 +60,43 @@ function TickParamsSelector({
   );
 }
 
-
-const dataset = [
-  {
-    london: 59,
-    paris: 57,
-    newYork: 86,
-    seoul: 21,
-    month: 'Jan',
-  },
-  {
-    london: 50,
-    paris: 52,
-    newYork: 78,
-    seoul: 28,
-    month: 'Fev',
-  },
-  {
-    london: 47,
-    paris: 53,
-    newYork: 106,
-    seoul: 41,
-    month: 'Mar',
-  },
-  {
-    london: 54,
-    paris: 56,
-    newYork: 92,
-    seoul: 73,
-    month: 'Apr',
-  },
-  {
-    london: 57,
-    paris: 69,
-    newYork: 92,
-    seoul: 99,
-    month: 'May',
-  },
-  {
-    london: 60,
-    paris: 63,
-    newYork: 103,
-    seoul: 144,
-    month: 'June',
-  },
-  {
-    london: 59,
-    paris: 60,
-    newYork: 105,
-    seoul: 319,
-    month: 'July',
-  },
-  {
-    london: 65,
-    paris: 60,
-    newYork: 106,
-    seoul: 249,
-    month: 'Aug',
-  },
-  {
-    london: 51,
-    paris: 51,
-    newYork: 95,
-    seoul: 131,
-    month: 'Sept',
-  },
-  {
-    london: 60,
-    paris: 65,
-    newYork: 97,
-    seoul: 55,
-    month: 'Oct',
-  },
-  {
-    london: 67,
-    paris: 64,
-    newYork: 76,
-    seoul: 48,
-    month: 'Nov',
-  },
-  {
-    london: 61,
-    paris: 70,
-    newYork: 103,
-    seoul: 25,
-    month: 'Dec',
-  },
-];
-
-const valueFormatter = (value) => `${value}mm`;
-
-const chartSetting = {
-  yAxis: [
-    {
-      label: 'rainfall (mm)',
-    },
-  ],
-  series: [{ dataKey: 'seoul', label: 'Seoul rainfall', valueFormatter }],
-  height: 300,
-  sx: {
-    [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-      transform: 'translateX(-10px)',
-    },
-  },
-};
-
 export default function Home() {
   const [tickPlacement, setTickPlacement] = React.useState('middle');
   const [tickLabelPlacement, setTickLabelPlacement] = React.useState('middle');
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(`${API_URL}`)
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const dataset = data.map(item => ({
+    name: item.athlete_full_name,
+    medals: item.athlete_medals,
+    birthYear: item.athlete_year_birth,
+    firstGame: item.first_game,
+    participations: item.games_participations,
+  }));
+
+  const chartSetting = {
+    yAxis: [
+      {
+        label: 'Games Participations',
+      },
+    ],
+    series: [{ dataKey: 'participations', label: 'Games Participations' }],
+    height: 300,
+    sx: {
+      [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+        transform: 'translateX(-10px)',
+      },
+    },
+  };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '75%' , flexDirection:'column'}}>
@@ -178,7 +109,7 @@ export default function Home() {
       <BarChart
         dataset={dataset}
         xAxis={[
-          { scaleType: 'band', dataKey: 'month', tickPlacement, tickLabelPlacement },
+          { scaleType: 'band', dataKey: 'name', tickPlacement, tickLabelPlacement },
         ]}
         {...chartSetting}
       />
