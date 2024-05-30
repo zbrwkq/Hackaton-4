6f09233a5924a9225e4711be441290bf6acf1476
unknown <bensalahabdelrrahman1@gmail.com>
2024-05-30 11:03:33 +0200

    route countries -->back

--- back/index.py
+++ back/index.py
@@ -16,9 +16,25 @@ engine = create_engine(db_url)
 @app.route('/')
 def index():
     with engine.connect() as connection:
-        result = connection.execute(text('SELECT * FROM Athlete LIMIT 1000'))  # Limiter les résultats
+        result = connection.execute(text('SELECT * FROM Medal LIMIT 1000'))  # Limiter les résultats
         rows = [dict(row) for row in result.mappings()]
     return jsonify(rows)
+
+@app.route('/predict_medal', methods=['POST'])
+def predict_medal():
+    from app.spark_model import predict_medal_type
+    predictions = predict_medal_type()
+    return jsonify(predictions)
+
+
+@app.route('/countries', methods=['GET'])
+def get_countries():
+    with engine.connect() as connection:
+        result = connection.execute(text('SELECT DISTINCT nation FROM Country_historical_medals'))
+        countries = [row['nation'] for row in result]
+    return jsonify(countries)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
