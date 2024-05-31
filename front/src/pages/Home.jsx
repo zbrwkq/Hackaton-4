c723598e41ff859fe7d6ec4036f2f5691c87e84d
joseph fremont <jomatf50@outlook.fr>
2024-05-31 03:03:14 +0200

    + -> change home page -> add horizontale graph, add image olympic game

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import GraphCountryHistoricalMedals from '../components/Graph/GraphCountryHistoricalMedals.jsx';
+import GraphMedals from '../components/Graph/GraphMedals.jsx';
 import '../App.css';
 
 export default function Home() {
   return (
     <div className="centered-container">
       <div className="inner-container">
-        <GraphCountryHistoricalMedals />
+        <img src="image/800px-Logo_JO_d'été_-_Paris_2024.svg.png"/>
+        <h1 style={{fontFamily:'N27-Regular', marginTop:60}}><b>Liste des compétiteurs</b></h1>
+        <GraphMedals />
       </div>
     </div>
   );
